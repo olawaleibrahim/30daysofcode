57e450800094f92836f6e6645d9ba307c96b5018
Olawale Ibrahim <54900088+olawaleibrahim@users.noreply.github.com>
2020-05-01 19:46:57 +0100

    Added try and except block

--- average.py
+++ average.py
@@ -1,11 +1,17 @@
 def get_average(string):
-    sum = 0
-    new = []
-    for i in string.split(' '):
-        new.append(int(i))
-    for i in new:
-        sum = sum + i
-    print(f'The average of the numbers is {sum/(len(new))}')
-    
+    try:
+        sum = 0
+        sum_list = []
+        for i in string.split(' '):
+            sum_list.append(int(i))
+        for i in sum_list:
+            sum = sum + i
+        print(f'The average of the numbers is {sum/(len(sum_list))}')
+    except SyntaxError:
+        print('Wrap inputs in parenthesis')
+    except ValueError:
+        print(sum_list)
+        print('Enter a valid string of numbers only')
 
-get_average('12 34 67')
+
+get_average('34 3 45')
